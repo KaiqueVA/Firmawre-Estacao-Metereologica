0e1ab6d05886e4370e980f2579f2da37d604d20a
KaiqueVA <kaique_x_@hotmail.com>
2025-09-26 03:44:50 +0000

    feat: initialize wind sensors and implement main application loop

--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
+#include "wind_sensors.h"
+#include "esp_log.h"
 
 
 void app_main(void)
-{
-    printf("Teste\n");
+{   
+
+    init_wind_sensors();
+    
+    while(1)
+    {
+        get_count_34();
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
 }
