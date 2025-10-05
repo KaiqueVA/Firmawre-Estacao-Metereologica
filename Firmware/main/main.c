cbc86e6c551c844be3e68981eca7b3b240441534
KaiqueVA <kaique_x_@hotmail.com>
2025-10-05 04:41:35 +0000

    feat: add rain gauge and refactor component name to include rain gauge

--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
-#include "wind_sensors.h"
+#include "wind_rain_sensors.h"
 #include "esp_log.h"
 
 
 void app_main(void)
 {   
 
-    init_wind_sensors();    
+    init_wind_rain_sensors();    
 }
