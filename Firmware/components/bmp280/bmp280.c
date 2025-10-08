6412d60fd8c609243aab0ee82e8a396041eaee36
KaiqueVA <kaique_x_@hotmail.com>
2025-10-08 16:13:00 +0000

    feat: add BMP280 control and configuration functions, and improve data reading

--- Firmware/components/bmp280/bmp280.c
+++ Firmware/components/bmp280/bmp280.c
@@ -30,6 +30,10 @@
 #define BMP280_REG_CALIB00         0x88  // T1 (LSB)
 #define BMP280_REG_CALIB25         0xA1  // Último coeficiente de calibração (H7 ou dig_P9)
 
+#define BMP280_OSRS_T_X2   (0b010 << 5)
+#define BMP280_OSRS_P_X16  (0b101 << 2)
+#define BMP280_MODE_NORMAL (0b11)
+
 typedef struct {
     uint16_t dig_T1;
     int16_t  dig_T2;
@@ -49,6 +53,7 @@ typedef struct{
     uint8_t *data;
     uint32_t length;
     bmp280_calib_t calibration_data;
+    int64_t t_fine;
 }bmp280_data_i2c_t;
 
 
@@ -58,6 +63,11 @@ bmp280_data_i2c_t bmp280_i2c;
 esp_err_t bmp280_get_id(void);
 esp_err_t bmp280_soft_reset(void);
 esp_err_t bmp280_get_calib_data(void);
+esp_err_t bmp280_ctrl_meas(void);
+esp_err_t bmp280_config(void);
+int32_t bmp280_compensate_T(int32_t adc_T);
+uint32_t bmp280_compensate_P(int32_t adc_P);
+
 
 
 esp_err_t bmp280_init(void)
@@ -87,7 +97,44 @@ esp_err_t bmp280_init(void)
         return ret;
     }
     ESP_LOGI("BMP280", "Calibration data loaded.");
+
+    ret = bmp280_ctrl_meas();
+    if(ret != ESP_OK)
+    {
+        ESP_LOGE("BMP280", "Failed to set ctrl_meas");
+        return ret;
+    }
+
+    ret = bmp280_config();
+    if(ret != ESP_OK)
+    {
+        ESP_LOGE("BMP280", "Failed to set config");
+        return ret;
+    }
+
+    ESP_LOGI("BMP280", "BMP280 initialized.");
+
     return ESP_OK;
+
+}
+
+
+
+
+esp_err_t bmp280_read_data(bmp280_data_t *data)
+{
+    uint8_t raw_data[6];
+    esp_err_t ret;
+    ret = hw_i2c_read(ADDR_BMP280, BMP280_REG_PRESS_MSB, raw_data, 6);
+    if(ret != ESP_OK)
+        return ret;
+    int32_t adc_p = (int32_t)((raw_data[0] << 12) | (raw_data[1] << 4) | (raw_data[2] >> 4));
+    int32_t adc_t = (int32_t)((raw_data[3] << 12) | (raw_data[4] << 4) | (raw_data[5] >> 4));
+
+    data->temperature = bmp280_compensate_T(adc_t)/100.0;
+    data->pressure = bmp280_compensate_P(adc_p)/25600.0;
+    ESP_LOGI("BMP280", "Temperature: %.2f C, Pressure: %.2f hPa", data->temperature, data->pressure);
+    return ret;
 
 }
 
@@ -140,3 +187,51 @@ esp_err_t bmp280_get_calib_data(void)
     
     
 }
+
+esp_err_t bmp280_ctrl_meas(void)
+{
+    uint8_t ctrl_meas = BMP280_OSRS_T_X2 | BMP280_OSRS_P_X16 | BMP280_MODE_NORMAL;
+    esp_err_t ret;
+    ret = hw_i2c_write(ADDR_BMP280, BMP280_REG_CTRL_MEAS, &ctrl_meas, 1);
+    return ret;
+}
+
+esp_err_t bmp280_config(void)
+{
+    uint8_t config = (0b100 << 5) | (0b100 << 2);
+    esp_err_t ret;
+    ret = hw_i2c_write(ADDR_BMP280, BMP280_REG_CONFIG, &config, 1);
+    return ret;
+}
+
+int32_t bmp280_compensate_T(int32_t adc_T)
+{
+    int32_t var1, var2;
+    var1 = ((((adc_T >> 3) - ((int32_t)bmp280_i2c.calibration_data.dig_T1 << 1))) * ((int32_t)bmp280_i2c.calibration_data.dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - ((int32_t)bmp280_i2c.calibration_data.dig_T1)) * ((adc_T >> 4) - ((int32_t)bmp280_i2c.calibration_data.dig_T1))) >> 12) * ((int32_t)bmp280_i2c.calibration_data.dig_T3)) >> 14;
+    bmp280_i2c.t_fine = var1 + var2;
+    float T = (bmp280_i2c.t_fine * 5 + 128) >> 8;
+    return T;
+}
+
+uint32_t bmp280_compensate_P(int32_t adc_P)
+{
+    int64_t var1, var2, p;
+    var1 = ((int64_t)bmp280_i2c.t_fine) - 128000;
+    var2 = var1 * var1 * (int64_t)bmp280_i2c.calibration_data.dig_P6;
+    var2 = var2 + ((var1 * (int64_t)bmp280_i2c.calibration_data.dig_P5) << 17);
+    var2 = var2 + (((int64_t)bmp280_i2c.calibration_data.dig_P4) << 35);
+    var1 = ((var1 * var1 * (int64_t)bmp280_i2c.calibration_data.dig_P3) >> 8) + ((var1 * (int64_t)bmp280_i2c.calibration_data.dig_P2) << 12);
+    var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)bmp280_i2c.calibration_data.dig_P1) >> 33;
+    if (var1 == 0) {
+        return 0; // evita divisão por zero
+    }
+    p = 1048576 - adc_P;
+    p = (((p << 31) - var2) * 3125) / var1;
+    var1 = (((int64_t)bmp280_i2c.calibration_data.dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+    var2 = (((int64_t)bmp280_i2c.calibration_data.dig_P8) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((int64_t)bmp280_i2c.calibration_data.dig_P7) << 4);
+    return p;
+
+}
+
--- Firmware/components/bmp280/include/bmp280.h
+++ Firmware/components/bmp280/include/bmp280.h
@@ -11,10 +11,7 @@ typedef struct{
 } bmp280_data_t;
 
 esp_err_t bmp280_init(void);
-void bmp280_read_data(bmp280_data_t *data);
-void bmp280_ctrl_meas(void);
-void bmp280_config(void);
-void bmp280_osrs_p(uint8_t oversampling);
+esp_err_t bmp280_read_data(bmp280_data_t *data);
 
 
 
