7bdefe9d90bf3bbfb23f64a58fdff89d9b430a9f
KaiqueVA <kaique_x_@hotmail.com>
2025-10-08 03:33:49 +0000

    feat: update CMakeLists and main.c to include bmp280 dependency and initialization

--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
-#include "wind_rain_sensors.h"
 #include "esp_log.h"
+#include "esp_err.h"
+#include "wind_rain_sensors.h"
+#include "bmp280.h"
 
 
 void app_main(void)
 {   
-
+    bmp280_init();
     init_wind_rain_sensors();    
 }
