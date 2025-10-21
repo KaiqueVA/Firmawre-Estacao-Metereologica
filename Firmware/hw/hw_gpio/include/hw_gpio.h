e1a93c218ffc609101613fbd0cd4bc032d15b910
KaiqueVA <kaique_x_@hotmail.com>
2025-10-21 03:37:31 +0000

    feat: add GPIO 17 ISR and update GPIO initialization for additional pin support

--- Firmware/hw/hw_gpio/include/hw_gpio.h
+++ Firmware/hw/hw_gpio/include/hw_gpio.h
@@ -11,12 +11,15 @@
 
 void isr_gpio_isr_34(void *arg);
 void isr_gpio_isr_35(void *arg);
+void isr_gpio_isr_17(void *arg);
 
 //==========================================
 
 //===========Protótipos de funções===========
 
 void hw_gpio_init(void);
+void hw_configure_gpio(gpio_num_t pin, gpio_mode_t mode, gpio_int_type_t intr_type);
+void hw_gpio_level_set(gpio_num_t pin, uint32_t level);
 
 //======================================
 #endif // HW_GPIO_H
